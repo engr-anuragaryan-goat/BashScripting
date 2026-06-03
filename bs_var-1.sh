f5c54a522a047c28f134aaf5b09cbb0b94b51155
Anurag Aryan <anuragaryan200608@gmail.com>
2026-06-03 09:36:56 +0530

    dealing with variable

---
+++ bs_var-1.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+# defining variables
+
+nmbr1=5
+nmbr2=2.6
+name="Anurag Aryan"
+
+# accessing values of variables
+
+echo " Integer is $nmbr1 "
+echo " Float point number is $nmbr2 "
+echo " Name is $name "
